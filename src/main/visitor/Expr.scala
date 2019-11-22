2b7fd4187231e971de7e9acdbbdaba71236f958b
fbariy <bariy.alex@gmail.com>
2019-11-22 20:07:16 +0300

    Reformat

--- src/main/visitor/Expr.scala
+++ src/main/visitor/Expr.scala
@@ -2,30 +2,25 @@ package visitor
 
 sealed trait Expr {
   def print: String
-
   def accept[T](visitor: ExprVisitor[T]): T
 }
 
 class Plus(val left: Expr, val right: Expr) extends Expr {
   def print: String = s"${left.print} + ${right.print}"
-
   def accept[T](visitor: ExprVisitor[T]): T = visitor.visitPlus(this)
 }
 
 class Literal(val number: Int) extends Expr {
   def print: String = number.toString
-
   def accept[T](visitor: ExprVisitor[T]): T = visitor.visitLiteral(this)
 }
 
 class UMinus(val expr: Expr) extends Expr {
   def print: String = s"-${expr.print}"
-
   def accept[T](visitor: ExprVisitor[T]): T = visitor.visitUMinus(this)
 }
 
 class Minus(val left: Expr, val right: Expr) extends Expr {
   def print: String = s"${left.print} - ${right.print}"
-
   def accept[T](visitor: ExprVisitor[T]): T = visitor.visitMinus(this)
 }
